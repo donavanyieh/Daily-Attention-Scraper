cfa6cc68594f3046b57ba850beb05ca2bd8f925c
Donavan Yieh <31071751+donavanyieh@users.noreply.github.com>
2026-01-03 15:13:11 +0800

    Update get_genai_summary.py

--- get_genai_summary.py
+++ get_genai_summary.py
@@ -14,30 +14,34 @@ You are a data science professor. You have a deep expertise in machine learning 
 <END OF CONTEXT>
 
 <TASK>
-You will be a list of reseach papers published today. Your job is to synthesize this into a TLDR for beginners to read
+You will be a list of research papers published today. The list contains multiple JSONs of information of research papers published today.
+Your job is to synthesize this into a TLDR for beginners to read
 - Brief Summary: A summary in a 120 words to 150 words of all papers. Just a simple paragraph, with no markdown formatting, e.g. no * and no #
 - Impact: Why this matters for a AI practitioner, in 50 to 60 words, in plain text e.g. no * and no #.
-- Exciting Topics: 5 to 7 key points where you think this has most impact on data science. Each topic is one to two sentence long, and must reference paper title name of any claims, and must have no markdown formatting, e.g. no * and no #
+- Exciting Topics: 3 to 5 key points where you think the provided research papers has most impact on data science. Each topic is one to two sentence long, and must reference paper title name of any claims, and must have no markdown formatting, e.g. no * and no #
 
 Be clear and mindful of your audience who are beginners, and make them excited about AI.
-It is very important that you reference paper TITLES, not paper id. Reason and think to use paper titles (typically string) instead of paper id (typically numbers with '.' in the middle). 
+It is very important that you reference paper TITLES.
+Reason and think to use paper titles (typically string) instead of paper id (typically numbers with '.' in the middle), or other paper metadata like tags
 <END OF TASK>
 
 <IMPORTANT>
-These 3 points are key to performing your task well:
+These 3 golden rules are key to performing your task well:
 1. This is STRICTLY a data summarization task. Your analysis and answer should be ONLY from the list of reseach papers information provided within this prompt.
-2. When making statements, make sure you reference the paper titles appropriately. This is so the user knows which paper to explore further. REFERENCE THE PAPER TITLE, NOT THE ID.
+2. When making statements, make sure you reference the paper titles appropriately. It is important you reference the paper titles instead of other paper metadata, so that the end user can search it up themselves. This is so the user knows which paper to explore further. REFERENCE THE PAPER TITLE.
 3. Think and reason beyond human capability and make sure your answer stays within the context of the research papers provided in this prompt
+
+These golden rules apply when coming up with the summary, impact, and exciting topics. Adhere to these golden rules at all times.
 </IMPORTANT>
 
-HERE IS THE LIST OF RESEARCH PAPERS
+HERE IS THE LIST OF RESEARCH PAPERS FOR YOUR ANALYSIS
 <research papers>
-<research_df>
+<research_json>
 <end of research papers>
 
 <OUTPUT FORMAT>
 Give your answer strictly in JSON format only, in the following format:
-{"summary": <summary string>, "impact": <impact string>, "exciting topics":<list of 5 to 7 key points of exciting new things STRICTLY FROM PROVIDED LIST OF RESEARCH PAPERS>}
+{"summary": <summary string>, "impact": <impact string>, "exciting topics":<list of 3 to 5 key points of exciting new things STRICTLY FROM PROVIDED LIST OF RESEARCH PAPERS, referencing PAPER TITLES. Follow instructions in TASK and IMPORTANT>}
 <END OF OUTPUT FORMAT>
 """.strip()
 
@@ -51,7 +55,7 @@ def get_model_response(df, prompt = PROMPT, api_key = API_KEY):
             {
                 "role": "user",
                 "content": [
-                    {"type": "text", "text": prompt.replace("<research_df>", f"{df}")},
+                    {"type": "text", "text": prompt.replace("<research_json>", f"{df.to_dict('records')}")},
                 ],
             }
         ],
